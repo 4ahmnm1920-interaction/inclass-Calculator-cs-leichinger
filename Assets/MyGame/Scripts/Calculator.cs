3a74bfedf551f17d32125d09b307981cd37f35d4
leoeichinger <eichinger.l@gmx.at>
2020-01-15 11:31:07 +0100

    copied aleks project

---
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public InputField ipf_number1;
+    public InputField ipf_number2;
+    public Text result;
+    public Button btn_add;
+    public Button btn_sub;
+    public Button btn_multiply;
+    public Button btn_divide;
+    public Button btn_pythagoras;
+    public Text text_operator;
+
+
+    public void OnAddBtnClick()
+    {
+        AddNumbersOnClick();
+        text_operator.text = "+";
+    }
+
+    public void OnSubBtnClick()
+    {
+        SubNumbersOnClick();
+        text_operator.text = "-";
+    }
+
+    public void OnMultiplyBtnClick()
+    {
+        MultiplyNumbersOnClick();
+        text_operator.text = "*";
+    }
+
+    public void OnDivideBtnClick()
+    {
+        DivideNumbersOnClick();
+        text_operator.text = ":";
+    }
+
+    public void OnPythagorasBtnClick()
+    {
+        var PythagorasResult = PythagorasNumbersOnClick();
+        text_operator.text = "pythagoras";
+        result.text = PythagorasResult.ToString();
+    }
+
+    public void AddNumbersOnClick()
+    {
+        float number1 = float.Parse(ipf_number1.text);
+        float number2 = float.Parse(ipf_number2.text);
+        float addResult = number1 + number2;
+        result.text = addResult.ToString();
+    }
+
+    public void SubNumbersOnClick()
+    {
+        float number1 = float.Parse(ipf_number1.text);
+        float number2 = float.Parse(ipf_number2.text);
+        float addResult = number1 - number2;
+        result.text = addResult.ToString();
+    }
+
+    public void MultiplyNumbersOnClick()
+    {
+        float number1 = float.Parse(ipf_number1.text);
+        float number2 = float.Parse(ipf_number2.text);
+        float addResult = number1 * number2;
+        result.text = addResult.ToString();
+    }
+
+    public void DivideNumbersOnClick()
+    {
+        float number1 = float.Parse(ipf_number1.text);
+        float number2 = float.Parse(ipf_number2.text);
+        float addResult = number1 / number2;
+        result.text = addResult.ToString();
+    }
+
+    public float PythagorasNumbersOnClick()
+    {
+        float number1 = float.Parse(ipf_number1.text);
+        float number2 = float.Parse(ipf_number2.text);
+        float aSquare = number1 * number1;
+        float bSquare = number2 * number2;
+        float squareResult = aSquare + bSquare;
+        float addResult = Mathf.Sqrt(squareResult);
+        return addResult;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Temp201914f12D
+# Unity2019114f1-2D-Template
+
+### Project description: 
+Simple c# Calculator script in Unity. You can add, subtract, divide, multiply numbers, and also do the Pythagoras Theoreme.
+
+### Development platform: 
+(macOS Catalina Ver. 10.15.1, Unity 2019 14f1, Visual Studio for Mac 7.7.4 (Build 1), SDK's none)
+Scripting Runtime Version: .NET 4.0
+
+### Target platform: 
+PC 
+
+### Necessary setup/execution steps: 
+Clone the project from github and add it via Unity Hub.
+
+### Third party material: 
+none
+
+### Project state: 
+done 100%
+ 
+
+### Lessons Learned:
+Teamwork exercise and how to make a calculator in Unity using c# and working with return values.
+
+© Copyright by
+Eichinger Leo
+Etzer Matteo
+Radić Aleksa
