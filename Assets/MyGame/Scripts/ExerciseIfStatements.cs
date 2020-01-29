15fa1422de1014c2f24d8a2f0bb44245f0a49c7b
leoeichinger <eichinger.l@gmx.at>
2020-01-29 10:01:38 +0100

    addet if statment practice

--- Assets/MyGame/Scripts/ExerciseIfStatements.cs
+++ Assets/MyGame/Scripts/ExerciseIfStatements.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class ExerciseIfStatements : MonoBehaviour
 {
+
     public bool condition;
+    public string text;
+    public string text2;
     void Start()
     {
-        if(condition)
+        
+    }
+
+    void Update() 
+    {
+        float wert = Random.Range(5f,15f);
+        
+        if(wert>10f)
         {
-            Debug.Log("Diese Aussage(Condition) ist richtig");
+            //Debug.Log("Diese Aussage(Condition) ist richtig");
         }
         else
         {
             Debug.Log("Jetzt ist falsh");
         }
+
+
+//Mit den && (und) Operator - Beide müssen richtig sein
+        if(text == text2 && condition)
+        {
+            //Debug.Log("Text entpsricht text2");
+        }
+        else
+        {
+           // Debug.Log("Text entspricht nicht Text 2");
+        }
+
+//mit dem || (oder) Operator muss eines der bieden richtig sein
+        if(text == text2 || condition)
+        {
+            Debug.Log("Einer der beiden Koditions ist richtig");
+        }
+        
     }
+
+
 }
